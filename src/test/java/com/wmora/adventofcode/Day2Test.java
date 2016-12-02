c48afdf08c722be87e293fce22b4015c11733008
William Mora <william.r.mora@gmail.com>
2016-12-02 07:45:19 -0500

    Day 2 Part 1

---
+++ src/test/java/com/wmora/adventofcode/Day2Test.java
@@ -0,0 +1,24 @@
+package com.wmora.adventofcode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Day2Test {
+
+    @Test
+    public void testBathroomCode() {
+        List<String> instructions = new ArrayList<>();
+        instructions.add("ULL");
+        instructions.add("RRDDD");
+        instructions.add("LURDL");
+        instructions.add("UUUUD");
+
+        Day2 day2 = new Day2();
+
+        assertEquals("1985", day2.getBathroomCode(instructions));
+    }
+}
