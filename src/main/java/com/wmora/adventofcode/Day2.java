90a9983ac22dec643379ca2f25b63834f955299f
William Mora <william.r.mora@gmail.com>
2016-12-02 07:56:24 -0500

    Day 2 Part 2

--- src/main/java/com/wmora/adventofcode/Day2.java
+++ src/main/java/com/wmora/adventofcode/Day2.java
@@ -37,6 +37,47 @@ class Day2 {
         return code;
     }
 
+    String getFancyBathroomCode(List<String> instructions) {
+        String code = "";
+        String[][] keypad = new String[][]{
+                new String[]{"X", "X", "1", "X", "X"},
+                new String[]{"X", "2", "3", "4", "X"},
+                new String[]{"5", "6", "7", "8", "9"},
+                new String[]{"X", "A", "B", "C", "X"},
+                new String[]{"X", "X", "D", "X", "X"}};
+
+        int row = 2;
+        int column = 0;
+
+        for (String line : instructions) {
+            for (int i = 0; i < line.length(); i++) {
+                String direction = String.valueOf(line.charAt(i));
+
+                if ("U".equals(direction) && row > 0) {
+                    if (!"X".equals(keypad[row - 1][column])) {
+                        row--;
+                    }
+                } else if ("D".equals(direction) && row < keypad.length - 1) {
+                    if (!"X".equals(keypad[row + 1][column])) {
+                        row++;
+                    }
+                } else if ("L".equals(direction) && column > 0) {
+                    if (!"X".equals(keypad[row][column - 1])) {
+                        column--;
+                    }
+                } else if ("R".equals(direction) && column < keypad[column].length - 1) {
+                    if (!"X".equals(keypad[row][column + 1])) {
+                        column++;
+                    }
+                }
+
+            }
+            code += keypad[row][column];
+        }
+
+        return code;
+    }
+
     public static void main(String[] args) {
         String input = "RDLRUUULRRDLRLLRLDDUDLULULDDULUDRRUURLRLLUULDURRULLRULDRRDLLULLRLLDRLDDRRRRLLRLURRRDRDULRDUULDDDULURUDDRRRUULUDRLLUUURLUDRUUUDRDUULLRLLUDDRURRDDDRDLUUURLRLLUDRURDUDUULDDLLRDURULLLURLDURLUUULDULDDULULLLRRUDLRUURDRDLLURLUDULDUUUURRLDLUDRULUDLDLLDRLDDDRRLLDUDLLRRDDDRLUDURLLLDRUDDLDDRRLUDRRDUDLRRLULDULURULDULUULDRLLDRUUDDRLLUDRULLRRRLRDLRLUDLRULDRDLRDRLRULUDUURRUUULLDDDDUDDLDDDDRRULRDLRDDULLDLDLLDLLDLLDRRDDDRDDLRRDDDRLLLLURRDLRRLDRURDDURDULDDRUURUDUDDDRDRDDRLRRLRULLDRLDLURLRLRUDURRRDLLLUDRLRDLLDDDLLUDRLDRRUUDUUDULDULLRDLUDUURLDDRUDR\n" +
                 "URULDDLDDUDLLURLUUUUUULUDRRRDDUDURDRUURLLDRURLUULUDRDRLLDRLDULRULUURUURRLRRDRUUUDLLLLRUDDLRDLLDUDLLRRURURRRUDLRLRLLRULRLRLRDLRLLRRUDDRLRUDULDURDLDLLLRDRURURRULLLDLLRRDRLLDUUDLRUUDDURLLLDUUDLRDDURRDRRULLDRLRDULRRLLRLLLLUDDDRDRULRRULLRRUUDULRRRUDLLUUURDUDLLLURRDDUDLDLRLURDDRRRULRRUDRDRDULURULRUDULRRRLRUDLDDDDRUULURDRRDUDLULLRUDDRRRLUDLRURUURDLDURRDUUULUURRDULLURLRUUUUULULLDRURULDURDDRRUDLRLRRLLLLDDUURRULLURURRLLDRRDDUUDLLUURRDRLLLLRLUDUUUDLRLRRLDURDRURLRLRULURLDULLLRRUUUDLLRRDUUULULDLLDLRRRDUDDLRULLULLULLULRU\n" +
@@ -52,6 +93,7 @@ class Day2 {
         }
 
         System.out.println(new Day2().getBathroomCode(instructions));
+        System.out.println(new Day2().getFancyBathroomCode(instructions));
     }
 
 }
