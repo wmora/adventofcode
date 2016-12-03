c996c10a61e40eff2a597bd89b64817681b2cd61
William Mora <william.r.mora@gmail.com>
2016-12-03 08:24:25 -0500

    Day 3 Part 2

--- src/main/java/com/wmora/adventofcode/Day3.java
+++ src/main/java/com/wmora/adventofcode/Day3.java
@@ -42,7 +42,40 @@ class Day3 {
         }
 
         System.out.println(new Day3().validTriangles(triangles));
-    }
 
+        triangles.clear();
+
+        scanner = new Scanner(new File("day3.txt"));
+
+        int insertionCount = 0;
+
+        while (scanner.hasNextLine()) {
+            String nextLine = scanner.nextLine();
+            Scanner lineScanner = new Scanner(nextLine);
+            boolean shouldCreateTriangle = triangles.isEmpty() || insertionCount == 3;
+
+            List<Integer> triangle1 = shouldCreateTriangle ? new ArrayList<>() : triangles.get(triangles.size() - 3);
+            List<Integer> triangle2 = shouldCreateTriangle ? new ArrayList<>() : triangles.get(triangles.size() - 2);
+            List<Integer> triangle3 = shouldCreateTriangle ? new ArrayList<>() : triangles.get(triangles.size() - 1 );
+
+            triangle1.add(lineScanner.nextInt());
+            triangle2.add(lineScanner.nextInt());
+            triangle3.add(lineScanner.nextInt());
+
+            if (shouldCreateTriangle) {
+                triangles.add(triangle1);
+                triangles.add(triangle2);
+                triangles.add(triangle3);
+            }
+
+            if (insertionCount == 3) {
+                insertionCount = 1;
+            } else {
+                insertionCount++;
+            }
+        }
+
+        System.out.println(new Day3().validTriangles(triangles));
+    }
 
 }
