b26f5a4f291174501663b31fdc1f0c0d9520b3ff
William Mora <william.r.mora@gmail.com>
2016-12-04 08:54:06 -0800

    Day 4 Part 1

---
+++ src/main/java/com/wmora/adventofcode/Day4.java
@@ -0,0 +1,95 @@
+package com.wmora.adventofcode;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+class Day4 {
+
+    int sumOfSectorIds(List<String> rooms) {
+        int sum = 0;
+
+        for (String room : rooms) {
+            if (isValidRoom(room)) {
+                sum += roomSectorId(room);
+            }
+        }
+
+        return sum;
+    }
+
+    private int roomSectorId(String room) {
+        int sectorIdStartIndex = room.lastIndexOf("-") + 1;
+        int sectorIdEndIndex = room.indexOf("[");
+        return Integer.parseInt(room.substring(sectorIdStartIndex, sectorIdEndIndex));
+    }
+
+    private boolean isValidRoom(String room) {
+        int checksumStartId = room.indexOf("[");
+        String checksum = room.substring(checksumStartId + 1, room.length() - 1);
+
+        int roomNameEndIndex = room.lastIndexOf("-");
+        String roomName = room.substring(0, roomNameEndIndex);
+
+
+        Map<String, Integer> occurrences = getAllCharacterOccurrencesMap(roomName);
+
+        for (int i = 0; i < checksum.length(); i++) {
+            String character = String.valueOf(checksum.charAt(i));
+
+            if (!occurrences.containsKey(character) && occurrences.size() >= checksum.length()) {
+                return false;
+            }
+
+            if (i > 0) {
+                String previousCharacter = String.valueOf(checksum.charAt(i - 1));
+                int previousCharacterOccurrences = occurrences.get(previousCharacter);
+                int currentCharacterOccurrences = occurrences.get(character);
+                if (previousCharacterOccurrences < currentCharacterOccurrences) {
+                    return false;
+                } else if (previousCharacterOccurrences == currentCharacterOccurrences) {
+                    if (previousCharacter.compareTo(character) > 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private Map<String, Integer> getAllCharacterOccurrencesMap(String roomName) {
+        Map<String, Integer> occurrences = new HashMap<>();
+
+        for (int i = 0; i < roomName.length(); i++) {
+            String character = String.valueOf(roomName.charAt(i));
+            if (!"-".equals(character) && !occurrences.containsKey(character)) {
+                occurrences.put(character, getOccurrences(character, roomName));
+            }
+        }
+        return occurrences;
+    }
+
+    private int getOccurrences(String character, String string) {
+        int occurrences = 0;
+        int index = string.indexOf(character);
+
+        while (index > -1) {
+            occurrences++;
+            index = string.indexOf(character, index + 1);
+        }
+
+        return occurrences;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> rooms = new ArrayList<>();
+
+        Scanner scanner = new Scanner(new File("day4.txt"));
+        while (scanner.hasNextLine()) {
+            rooms.add(scanner.nextLine());
+        }
+
+        System.out.println(new Day4().sumOfSectorIds(rooms));
+    }
+}
---
+++ src/test/java/com/wmora/adventofcode/Day4Test.java
@@ -0,0 +1,23 @@
+package com.wmora.adventofcode;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Day4Test {
+
+    @Test
+    public void testSumOfSectorIds() {
+        List<String> rooms = new ArrayList<>();
+        rooms.add("aaaaa-bbb-z-y-x-123[abxyz]");
+        rooms.add("a-b-c-d-e-f-g-h-987[abcde]");
+        rooms.add("not-a-real-room-404[oarel]");
+        rooms.add("totally-real-room-200[decoy]");
+
+        assertEquals(1514, new Day4().sumOfSectorIds(rooms));
+    }
+
+}
