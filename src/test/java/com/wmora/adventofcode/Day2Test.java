83a6f6ad4ba27f40304194333abaef8507a2a7ea
William Mora <william.r.mora@gmail.com>
2016-12-06 18:26:03 -0800

    Add test case for part 2

--- src/test/java/com/wmora/adventofcode/Day2Test.java
+++ src/test/java/com/wmora/adventofcode/Day2Test.java
@@ -21,4 +21,17 @@ public class Day2Test {
 
         assertEquals("1985", day2.getBathroomCode(instructions));
     }
+
+    @Test
+    public void testFancyBathroomCode() {
+        List<String> instructions = new ArrayList<>();
+        instructions.add("ULL");
+        instructions.add("RRDDD");
+        instructions.add("LURDL");
+        instructions.add("UUUUD");
+
+        Day2 day2 = new Day2();
+
+        assertEquals("5DB3", day2.getFancyBathroomCode(instructions));
+    }
 }
