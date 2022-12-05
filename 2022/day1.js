e786a91c0c9a7cf74569be24e4c8b8d21adc5d8b
William Mora <william.r.mora@gmail.com>
2022-12-05 08:52:40 -0500

    Add day 1

---
+++ 2022/day1.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const run = async function() {
+    const data = readline.createInterface({
+        input: fs.createReadStream('day1input.txt'),
+        // output: process.stdout,
+        console: false
+    });
+    
+    let maxCalories = 0;
+    let elfCalories = 0;
+
+    for await (const line of data) {
+        if (!line) {
+            if (elfCalories > maxCalories) {
+                maxCalories = elfCalories;
+            }
+            elfCalories = 0;
+        } else {
+            elfCalories += Number(line);
+        }
+      }
+    
+    if (elfCalories > maxCalories) {
+        maxCalories = elfCalories;
+    }
+        
+    console.log(maxCalories);
+}
+
+run();
