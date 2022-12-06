f5a73a800a18868a2661cdaa9c4d308f0737c2fb
William Mora <william.r.mora@gmail.com>
2022-12-06 08:39:29 -0500

    Day 6

--- 2022/day6-part2.js
+++ 2022/day6-part2.js
@@ -2,15 +2,12 @@ const fs = require('fs');
 
 const data = fs.readFileSync('day6input.txt', { encoding: 'utf8', flag: 'r' }).split('\n').filter(Boolean);
 
-let startOfMessageMarker = -1;
-
 data.forEach(line => {
     for (let index = 14; index < line.length; index++) {
-        const lastFourteen = [...new Set(line.substring(index - 14, index).split(''))].join('');
+        const lastFourteen = new Set(line.substring(index - 14, index));
 
-        if (lastFourteen.length === 14 && lastFourteen.indexOf(line.charAt(index)) < 0) {
-            startOfMessageMarker = index;
-            console.log(startOfMessageMarker);
+        if (lastFourteen.size === 14) {
+            console.log(index);
             break;
         }
     }
--- 2022/day6.js
+++ 2022/day6.js
@@ -2,15 +2,12 @@ const fs = require('fs');
 
 const data = fs.readFileSync('day6input.txt', { encoding: 'utf8', flag: 'r' }).split('\n').filter(Boolean);
 
-let startOfPacketMarker = -1;
-
 data.forEach(line => {
     for (let index = 4; index < line.length; index++) {
-        const lastFour = [...new Set(line.substring(index - 4, index).split(''))].join('');
+        const lastFour = new Set(line.substring(index - 4, index));
 
-        if (lastFour.length === 4) {
-            startOfPacketMarker = index;
-            console.log(startOfPacketMarker);
+        if (lastFour.size === 4) {
+            console.log(index);
             break;
         }
     }
